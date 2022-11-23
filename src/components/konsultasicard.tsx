c3f52bcc604603a7c2be3443dce0d72042281e82
desipuspitasari <desiipuspitasari98@gmail.com>
2022-11-23 19:38:22 +0700

    Update konsultasi keuangan

---
+++ src/components/konsultasicard.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FC } from "react";
+import Image from "next/image";
+import card from "../../public/images/card.png";
+import ButtonLink from "../components/buttonlink";
+
+const KonsultasiCard: FC<{
+  logo: any;
+  title: string;
+  content: string;
+}> = ({ logo, title, content }) => {
+  return (
+    <div className="bg-white pb-6 h-full border p-4 rounded-xl  drop-shadow-xl w-full ">
+      <div className="w-full  overflow-hidden rounded-2xl  aspect-square relative">
+        <Image src={logo} height={48} alt="" className="mb-6" />
+        <div className="text-blue-primary-500 text-2xl mb-2  font-black">
+          {title}
+        </div>
+        <div className=" text-justify">{content}</div>
+      </div>
+    </div>
+  );
+};
+export default KonsultasiCard;
