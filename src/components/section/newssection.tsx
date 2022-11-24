2717f35425c9457698a02d5a331349be3492c067
qyubitmetaverse <118708829+onezeno717@users.noreply.github.com>
2022-11-24 21:55:53 +0700

    update 24 11 2022

--- src/components/section/newssection.tsx
+++ src/components/section/newssection.tsx
@@ -1,11 +1,13 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import ButtonPrimary from "../../components/buttonprimary";
+import { Tab } from "@headlessui/react";
 
 import card from "../../../public/images/card.png";
 import Card from "../card";
 import ButtonSecondary from "../../components/buttonsecondary";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const news = [
   {
@@ -36,67 +38,120 @@ const news = [
   },
 ];
 
+const event = [
+  {
+    title: "SF Capital Events",
+    content:
+      "SFAST didesain sesuai dengan kebutuhan investor. Dengan tampilan yang easy.",
+    date: "February 17, 2021",
+    href: "#",
+    image: card,
+  },
+
+  {
+    title: "SF Capital Events",
+    content:
+      "SFAST didesain sesuai dengan kebutuhan investor. Dengan tampilan yang easy.",
+    date: "February 17, 2021",
+    href: "#",
+    image: card,
+  },
+
+  {
+    title: "SF Capital Events",
+    content:
+      "SFAST didesain sesuai dengan kebutuhan investor. Dengan tampilan yang easy.",
+    date: "February 17, 2021",
+    href: "#",
+    image: card,
+  },
+];
+
+const SlideContent = ({ items }: any) => {
+  return (
+    <>
+      <Swiper
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+        direction="horizontal"
+        className="flex w-full"
+        spaceBetween={50}
+        slidesPerView={3}
+      >
+        {items.map((item: any, index: number) => (
+          <SwiperSlide key={index}>
+            <Card
+              title={item.title}
+              content={item.content}
+              date={item.date}
+              href={item.href}
+              image={item.image}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
 const NewsSection: FC = () => {
   return (
-    <section className="flex py-14 w-full flex-col bg-white">
+    <Tab.Group as="div" className="flex py-14 w-full flex-col bg-white">
       <div className="w-full max-w-6xl space-y-8 flex-col flex mx-auto">
-        <div className="flex text-blue-primary-500 font-open-sans text-justify justify-between gap-x-6">
-          <div className="text-[32px] font-black">
+        <div className="grid grid-cols-1 lg:grid-cols-2 text-blue-primary-500 font-open-sans">
+          <div className="text-[32px] font-black mb-2">
             Berita dan Kegiatan Terbaru
           </div>
-          <div className="self-end flex space-x-2">
-            <div>
-              <ButtonPrimary
-                href="#_"
-                onClick={() => console.log("rilis media")}
-              >
-                Rilis Media
-              </ButtonPrimary>
-            </div>
-            <div>
-              <ButtonPrimary href="#">Internal</ButtonPrimary>
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full flex-col  ">
-          <Swiper
-            breakpoints={{
-              0: {
-                slidesPerView: 1,
-                spaceBetween: 30,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 30,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-            }}
-            direction="horizontal"
-            className="flex w-full"
-            spaceBetween={50}
-            slidesPerView={3}
+          <Tab.List
+            as="div"
+            className="flex justify-center lg:justify-end space-x-2"
           >
-            {news.map((i, index) => (
-              <SwiperSlide key={index}>
-                <Card
-                  title={i.title}
-                  content={i.content}
-                  date={i.date}
-                  href={i.href}
-                  image={i.image}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+            <Tab
+              as="button"
+              className="bg-blue-primary-500 font-open-sans text-base flex px-6 py-4 w-[262px] max-w-[262px] text-white group justify-center rounded-full "
+            >
+              <div className="group-focus:border-b group-focus:border-white  ">
+                Rilis Media
+              </div>
+              <FiArrowUpRight className="w-0 h-0 group-hover:w-4 group-hover:h-4 group-hover:transition-all group-focus:w-0 group-focus:h-0 transition-all" />
+            </Tab>
+            <Tab
+              as="button"
+              className="bg-blue-primary-500 font-open-sans text-base flex px-6 py-4 w-[262px] max-w-[262px] text-white group justify-center rounded-full "
+            >
+              <div className="group-focus:border-b group-focus:border-white  ">
+                Internal
+              </div>
+              <FiArrowUpRight className="w-0 h-0 group-hover:w-4 group-hover:h-4 group-hover:transition-all group-focus:w-0 group-focus:h-0 transition-all" />
+            </Tab>
+          </Tab.List>
         </div>
+        <Tab.Panels as="div" className="flex w-full">
+          <Tab.Panel as={Fragment}>
+            <SlideContent items={news} />
+          </Tab.Panel>
+          <Tab.Panel as={Fragment}>
+            <SlideContent items={event} />
+          </Tab.Panel>
+        </Tab.Panels>
+
         <div className="self-end">
           <ButtonSecondary href="#">Lihat Semua</ButtonSecondary>
         </div>
       </div>
-    </section>
+    </Tab.Group>
   );
 };
 
