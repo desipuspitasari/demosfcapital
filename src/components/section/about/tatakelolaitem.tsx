893fb91414f48665aee5473586c9d7777fae0a0e
qyubitmetaverse <118708829+onezeno717@users.noreply.github.com>
2022-11-25 22:13:09 +0700

    update tatakelola perusahaan

---
+++ src/components/section/about/tatakelolaitem.tsx
@@ -0,0 +1,494 @@
+export const Komiteaudit = () => {
+  return (
+    <>
+      <p>
+        Perseroan telah memiliki Piagam Komite Audit sebagaimana diatur dalam
+        POJK No. 55/ 2015 dengan ditetapkannya Piagam Komite Audit oleh Dewan
+        Komisaris Perseroan pada tanggal 15 Februari 2019.
+      </p>
+      <p>
+        Perseroan telah membentuk Komite Audit sebagaimana diatur dalam POJK No.
+        55/ 2015, berdasarkan Surat Keputusan Dewan Komisaris Perseroan No.
+        0002/SFC-IPO/II/2019 tentang Pengangkatan Ketua dan Anggota Komite Audit
+        tanggal 15 Februari 2019.
+      </p>
+      <p>
+        Komite ini bertugas untuk memberikan pendapat profesional yang
+        independen kepada Dewan Komisaris Perseroan terhadap laporan atau
+        hal-hal yang disampaikan oleh Direksi Perseroan kepada Dewan Komisaris
+        Perseroan serta mengidentifikasikan hal-hal yang memerlukan perhatian
+        Dewan Komisaris Perseroan, yang antara lain meliputi :
+      </p>
+      <p>
+        A. Membuat rencana kegiatan tahunan yang disetujui oleh Dewan Komisaris
+        Perseroan
+      </p>
+      <p>
+        B. Melakukan penelaahan atas informasi keuangan yang akan dikeluarkan
+        Perseroan seperti laporan keuangan, proyeksi, dan informasi keuangan
+        lainnya
+      </p>
+      <p>
+        C. Melakukan penelaahan atas ketaatan Perseroan terhadap peraturan
+        perundang-undangan yang berhubungan dengan kegiatan Perseroan
+      </p>
+      <p>
+        D. Melakukan penelaahan/penilaian atas pelaksanaan pemeriksaan oleh
+        auditor internal dan mengawasi pelaksanaan tindak lanjut oleh Direksi
+        Perseroan atas semua temuan auditor internal
+      </p>
+      <p>
+        E. Melakukan penelahaan dan melaporkan kepada Dewan Komisaris Perseroan
+        atas pengaduan yang berkaitan dengan Perseroan
+      </p>
+      <p>
+        F. Menjaga kerahasiaan dengan Akuntan Publik atas data dan informasi
+        Perseroan
+      </p>
+      <p>
+        G. Mengawasi hubungan dengan Akuntan Publik dan mengadakan
+        rapat/pembahasan dengan Akuntan Publik
+      </p>
+      <p>
+        H. Membuat, mengkaji, dan memperbaharui pedoman Komite Audit bila perlu
+      </p>
+      <p>
+        I. Memberikan pendapat independen apabila terjadi perbedaan pendapat
+        antara manajemen dan Akuntan Publik atas jasa yang diberikan
+      </p>
+      <p>
+        J. Memberikan rekomendasi kepada Dewan Komisaris Perseroan mengenai
+        penunjukan Akuntan Publik, didasarkan pada independensi, ruang lingkup
+        penugasan, dan fee
+      </p>
+      <p>
+        K. Melakukan penelaahan terhadap aktifitas pelaksanaan manajemen resiko
+        yang dilakukan oleh Direksi Perseroan, jika Perseroan tidak memiliki
+        fungsi pemantau resiko yang dilakukan oleh Direksi Perseroan, jika
+        Perseroan tidak memiliki fungsi pemantau resiko di bawah Dewan Komisaris
+        Perseroan
+      </p>
+      <p>
+        L. Menelaah dan memberikan saran kepada Dewan Komisaris Perseroan
+        terkait potensi benturan kepentingan Perseroan
+      </p>
+    </>
+  );
+};
+
+export const Komitenominasi = () => {
+  return (
+    <>
+      <p>
+        Perseroan telah memiliki Komite Nominasi dan Remunerasi dan telah
+        memenuhi ketentuan <strong>POJK No. 34/2014</strong> berdasarkan
+        Keputusan Dewan Komisaris yang tertuang dalam Piagam Komite Nominasi dan
+        Remunerasi Perseroan tanggal 15 Februari 2019 berdasarkan Surat
+        Keputusan Dewan Komisaris
+        <strong>Perseroan No. 0004/SFC-IPO/II/2019</strong> tentang Pengangkatan
+        Anggota Komite Nominasi dan Remunerasi.
+      </p>
+
+      <p>
+        Dalam menjalankan fungsinya, Komite Nominasi dan Remunerasi memiliki
+        tugas dan tanggung jawab antara lain sebagai berikut:
+      </p>
+
+      <p>
+        <strong>A. Tugas</strong>
+      </p>
+
+      <p>1. Terkait Fungsi Nominasi</p>
+
+      <p>
+        A. Menyusun dan memberikan rekomendasi kepada Dewan Komisaris terkait
+        penentuan:
+      </p>
+
+      <p>
+        i. Komposisi jabatan anggota Direksi dan/atau Dewan Komisaris Perseroan
+      </p>
+
+      <p>
+        ii. Kebijakan dan kriteria yang diperlukan dalam proses nominasi anggota
+        Direksi dan/atau Dewan Komisaris Perseroan
+      </p>
+
+      <p>
+        iii. Kebijakan evaluasi anggota Direksi dan/atau Dewan Komisaris
+        Perseroan iv. Program pengembangan kemampuan anggota Direksi dan/atau
+        Dewan Komisaris Perseroan
+      </p>
+
+      <p>
+        B. Memberikan usulan mengenai calon anggota Dewan Komisaris dan/atau
+        Direksi kepada Dewan Komisaris untuk disampaikan kepada RUPS Perseroan
+      </p>
+
+      <p>
+        C. Menentukan kriteria untuk diimplementasikan dalam mengindentifikasi
+        para calon, memeriksa dan menyetujui calon anggota Direksi dan/atau
+        Dewan Komisaris Perseroan, dan dalam melakukan hal tersebut Komite
+        Nominasi dan Remunerasi Perseroan akan menerapkan prinsip bahwa setiap
+        calon mampu dan layak untuk jabatan kedudukan yang bersangkutan dan
+        merupakan calon yang memenuhi syarat untuk posisi atau kedudukan
+        tersebut dengan pengalaman, kemampuan dan faktor-faktor relevan lainnya
+      </p>
+
+      <p>
+        D. Menjalankan prosedur nominasi bagi anggota Dewan Komisaris dan/atau
+        Direksi sebagaimana dimaksud di atas
+      </p>
+
+      <p>2. Terkait Fungsi Remunerasi</p>
+
+      <p>
+        A. Melakukan evaluasi terhadap kebijakan remunerasi serta evaluasi
+        terhadap kesesuaian dengan pelaksanaan kebijakan remunerasi dari waktu
+        ke waktu
+      </p>
+
+      <p>
+        B. Memberikan rekomendasi kepada Dewan Komisaris mengenai struktur,
+        kebijakan dan besaran remunerasi bagi Dewan Komisaris dan Direksi untuk
+        disampaikan dalam RUPS Perseroan
+      </p>
+
+      <p>
+        C. Memberikan rekomendasi kepada Dewan Komisaris mengenai kerangka
+        kebijakan remunerasi bagi pegawai secara keseluruhan yang sebelumnya
+        telah disetujui oleh Direksi. Rekomendasi tersebut (jika ada)
+        selanjutnya akan disampaikan oleh Dewan Komisaris kepada Direksi
+      </p>
+
+      <p>
+        D. Membantu Dewan Komisaris melakukan penilaian kinerja anggota Direksi
+        dan/atau Dewan Komisaris berdasarkan kriteria yang telah disusun sebagai
+        bahan evaluasi
+      </p>
+
+      <p>
+        E. Komite wajib menjalankan prosedur remunerasi bagi anggota Dewan
+        Komisaris dan/atau Direksi Perseroan sebagai berikut:
+      </p>
+
+      <p>
+        i. Menyusun struktur remunerasi berupa gaji, honorarium, insentif dan
+        tunjangan yang bersifat tetap dan variabel
+      </p>
+
+      <p>ii. Menyusun kebijakan atas struktur remunerasi</p>
+
+      <p>iii. Menyusun besaran atas struktur remunerasi</p>
+
+      <p>
+        F. Struktur, kebijakan, dan besaran remunerasi sebagaimana yang dimaksud
+        di atas harus memiliki kelayakan, kepatutan, serta tolok ukur yang wajar
+        dengan mempertimbangkan:
+      </p>
+
+      <p>
+        i. Remunerasi yang berlaku dalam sektor industri kegiatan usaha
+        Perseroan dari waktu ke waktu
+      </p>
+
+      <p>ii. Kinerja keuangan dan pemenuhan kewajiban keuangan Perseroan</p>
+
+      <p>
+        iii. Prestasi kerja individual anggota Dewan Komisaris dan/atau Direksi
+        Perseroan
+      </p>
+
+      <p>
+        iv. Kinerja, tugas, tanggung jawab, dan wewenang anggota Dewan Komisaris
+        dan/atau Direksi Perseroan
+      </p>
+
+      <p>
+        v. Tujuan dan pencapaian kinerja jangka pendek atau panjang yang sesuai
+        dengan strategi Perseroan
+      </p>
+
+      <p>
+        vi. Keseimbangan tunjangan yang bersifat tetap dan variatif dengan
+        memperhatikan kelayakan dan keseluruhan remunerasi bagi Dewan
+      </p>
+
+      <p>vii. Komisaris dan/atau Direksi Perseroan</p>
+
+      <p>
+        G. Komite dapat mempertimbangkan masukan dari anggota Direksi maupun
+        anggota Dewan Komisaris lainnya terkait kebijakan yang akan
+        direkomendasikan
+      </p>
+
+      <p>
+        H. Struktur, kebijakan dan besaran Remunerasi harus dievaluasi oleh
+        Komite minimal 1 kali dalam setahun
+      </p>
+
+      <p>
+        I. Melaksanakan tugas lain yang diberikan Dewan Komisaris yang berkaitan
+        dengan remunerasi sesuai dengan ketentuan yang berlaku
+      </p>
+
+      <p>
+        3. Benturan Kepentingan Dalam hal Ketua dan/atau Anggota Komite Nominasi
+        dan Remunerasi Perseroan memiliki benturan kepentingan dengan usulan
+        yang direkomendasikan, maka dalam usulan tersebut wajib diungkapkan
+        adanya benturan kepentingan serta pertimbangan-pertimbangan yang
+        mendasari usulan tersebut.
+      </p>
+
+      <p>
+        <strong>B. Tanggung Jawab</strong>
+      </p>
+
+      <p>
+        1. Komite Nominasi dan Remunerasi Perseroan bertanggung jawab kepada
+        Dewan Komisaris Perseroan
+      </p>
+
+      <p>
+        2. Pertanggungjawaban Komite Nominasi dan Remunerasi Perseroan
+        disampaikan kepada Dewan Komisaris Perseroan dalam bentuk laporanlaporan
+        sebagai berikut:
+      </p>
+
+      <p>i. Laporan hasil rapat (risalah rapat)</p>
+
+      <p>
+        ii. Laporan kinerja dan pelaksanaan kegiatan triwulanan Komite Nominasi
+        dan Remunerasi Perseroan
+      </p>
+
+      <p>
+        iii. Laporan kinerja dan pelaksanaan kegiatan tahunan Komite Nominasi
+        dan Remunerasi Perseroan iv. Laporan hasil kunjungan lapangan yang
+        antara lain berisi temuan atau fakta lapangan, evaluasi, analisa,
+        kesimpulan dan saran, jika ada
+      </p>
+
+      <p>
+        3. Ketua dan/atau Anggota Komite Nominasi dan Remunerasi Perseroan yang
+        masih, atau yang sudah tidak lagi menjabat, wajib menjaga kerahasiaan
+        dokumen, data, dan informasi Perseroan yang diperoleh sewaktu menjabat,
+        baik dari pihak internal maupun pihak eksternal dan hanya digunakan
+        untuk kepentingan pelaksanaan tugasnya
+      </p>
+
+      <p>
+        4. Ketua dan/atau Anggota Komite Nominasi dan Remunerasi Perseroan
+        dilarang menyalahgunakan informasi penting yang berkaitan dengan
+        Perseroan untuk kepentingan pribadi
+      </p>
+
+      <p>
+        5. Ketua dan/atau Anggota Komite Nominasi dan Remunerasi Perseroan dalam
+        melaksanakan tugas dan tanggungjawabnya wajib mentaati ketentuan
+        peraturan yang berlaku dan COBP Perseroan serta dilarang mengambil
+        keuntungan pribadi baik secara langsung maupun tidak langsung dari
+        kegiatan Perseroan selain honorarium berikut fasilitas dan tunjangan
+        lainnya
+      </p>
+
+      <p>
+        <strong>Profil Komite Nominasi dan Remunerasi</strong>
+      </p>
+
+      <p>Ketua : Harun Pandapotan</p>
+
+      <p>Anggota : Linawati Ruli</p>
+    </>
+  );
+};
+
+export const Sekretaris = () => {
+  return (
+    <>
+      <p>
+        Sekretaris Perusahaan bertanggung jawab langsung kepada Direksi dan
+        bersifat independen terhadap unit kerja lainnya. Pengangkatan Sekretaris
+        Perusahaan PT Surya Fajar Capital Tbk dilakukan mengacu pada Peraturan
+        OJK No.35/POJK.04/2014 tentang Sekretaris Perusahaan Emiten atau
+        Perusahaan Publik yang menggantikan Peraturan Bapepam Nomor IX.I.4
+        tentang Pembentukan Sekretaris Perusahaan.
+      </p>
+      <p>
+        Berdasarkan Surat No. 0013/SFC-IPO/III/2019 tanggal 6 Maret 2019,
+        Perseroan telah menunjuk EVIE FENIYANTI sebagai Sekretaris Perusahaan
+        yang menjalankan tugas-tugas Sekretaris Perusahaan. Profil dari Evie
+        Feniyanti dapat dilihat pada bagian Direksi
+      </p>
+      <p>
+        Adapun fungsi dan/atau tanggung jawab dari Sekretaris Perusahaan
+        sebagaimana diatur dalam POJK No. 35/2014 tentang Emiten atau Perusahaan
+        Publik, antara lain:
+      </p>
+      <p>
+        1. Memberikan masukan kepada Direksi Perseroan untuk mematuhi
+        ketentuan–ketentuan yang berlaku, termasuk tapi tidak terbatas
+        Undang-undang nomor 40 tahun 2007 tentang Perseroan Terbatas,
+        Undang-undang nomor 8 tahun 1995 tentang Pasar Modal serta
+        peraturan-peraturan yang berlaku di Republik Indonesia dan sesuai dengan
+        norma-norma corporate governance secara umum
+      </p>
+      <p>
+        2. Mengikuti perkembangan Pasar Modal khususnya peraturan-peraturan yang
+        berlaku di bidang Pasar Modal
+      </p>
+      <p>
+        3. Sebagai penghubung antara dengan Otoritas Jasa Keuangan, Bursa Efek
+        Indonesia, stakeholder, dan masyarakat
+      </p>
+      <p>4. Memelihara hubungan yang baik antara Perseroan dengan media masa</p>
+      <p>
+        5. Memberikan pelayanan kepada masyarakat (pemodal) atas setiap
+        Informasi yang dibutuhkan pemodal berkaitan dengan kondisi Perseroan
+      </p>
+      <p>
+        6. Melaksanakan kegiatan-kegiatan yang mendukung kegiatan Perseroan
+        tersebut di atas antara lain Laporan Tahunan, Rapat Umum Pemegang Saham,
+        Keterbukaan Informasi, dan lain-lain sebagainya
+      </p>
+      <p>
+        7. Mempersiapkan praktik Good Corporate Governance (GCG) di lingkungan
+        Perseroan
+      </p>
+      <p>
+        8. Menjaga dan mempersiapkan dokumentasi Perseroan, termasuk notulen
+        dari Rapat Direksi dan Rapat Dewan Komisaris serta hal-hal terkait
+      </p>
+    </>
+  );
+};
+
+export const Unitauditinternal = () => {
+  return (
+    <>
+      <p>
+        Perseroan telah memiliki Piagam Audit Internal sebagaimana diatur dalam
+        POJK No. 56/ 2015 dengan ditetapkannya Unit Audit Internal oleh Direksi
+        Perseroan pada tanggal 15 Februari 2019 melalui Surat Keputusan Direksi
+        Perseroan No. 0003/SFC-IPO/II/2019 dengan persetujuan Dewan Komisaris
+        Perseroan.
+      </p>
+
+      <p>
+        Piagam Audit Internal ini berisikan fungsi dan ruang lingkup Unit Audit
+        Internal (&ldquo;UAI&rdquo;) dalam memberikan jasa assurance dan
+        consulting yang independen obyektif guna memberikan nilai tambah dan
+        perbaikan operasional Perseroan. UAI membantu Perseroan dalam mencapai
+        tujuannya melalui penggunaan metode yang sistematis dalam mengevaluasi
+        dan meningkatkan efektivitas risk management, internal control, dan
+        governance processes.
+      </p>
+
+      <p>
+        <strong>Tugas Unit Internal Audit</strong>
+      </p>
+
+      <p>
+        1. Membantu tugas Direktur Utama dan Dewan Komisaris Perseroan,
+        khususnya Komite Audit Perseroan yang ditunjuk oleh Dewan Komisaris
+        Perseroan, dalam melakukan pengawasan dan evaluasi atas pelaksanaan
+        pengendalian internal dan manajemen resiko agar sesuai dengan kebijakan
+        Perseroan
+      </p>
+
+      <p>
+        2. Menyusun dan melaksanakan rencana Audit Internal Tahunan Perseroan
+      </p>
+
+      <p>
+        3. Mengkaji independensi, efisiensi dan efektifitas semua fungsi
+        manajemen dalam Perseroan
+      </p>
+
+      <p>
+        4. Menilai efektifitas system pengendalian internal, termasuk kepatuhan
+        terhadap kebijakan, prosedur, pedoman, dan limit-limit yang telah
+        ditetapkan
+      </p>
+
+      <p>
+        5. Menilai sistem pelaporan serta mengkaji atas keakuratan dan ketetapan
+        waktu penyampaian laporan kepada manajemen
+      </p>
+
+      <p>
+        6. Menilai kelayakan dan kewajaran pedoman dan perlakukan akuntansi yang
+        digunakan dan menguji ketaatan terhadap kebijakan dan pedoman akuntansi
+        yang telah ditetapkan
+      </p>
+
+      <p>
+        7. Menyelenggarakan Audit Internal secara efektif dengan melakukan
+        current audit, regular audit, maupun special audit. Pelaksanaan Audit
+        Internal tersebut harus didukung oleh Auditor yang independen, kompeten
+        dan professional
+      </p>
+
+      <p>
+        8. Melaporkan hasil temuan pemeriksaan secara langsung kepada Direktur
+        Utama dan Dewan Komisaris Perseroan (melalui Komite Audit Perseroan)
+      </p>
+
+      <p>
+        9. Melakukan pemeriksaan dan penilaian atas efisiensi dan efektivitas di
+        bidang keuangan, akuntansi, operasional, sumber daya manusia, pemasaran,
+        teknologi informasi, dan kegiatan lainnya
+      </p>
+
+      <p>
+        10. Memberikan saran perbaikan dan informasi yang objektif tentang
+        kegiatan yang diperiksa di semua tingkat manajemen
+      </p>
+
+      <p>11. Bekerjasama dengan Komite Audit Perseroan</p>
+
+      <p>
+        12. Menyusun program untuk mengevaluasi mutu kegiatan Audit Internal
+        yang dilakukan
+      </p>
+
+      <p>
+        13. Melakukan investigasi apabila terjadi indikasi penipuan, penggelapan
+        dan indikasi-indikasi lainnya yang merugikan Perseroan
+      </p>
+
+      <p>
+        14. Dalam melaksanakan tugasnya, Unit Audit Internal wajib mematuhi
+        peraturan perundang-undangan yang berlaku
+      </p>
+
+      <p>
+        <strong>Profil Audit Internal</strong>
+      </p>
+
+      <p>Auditor : Meilin Arifin</p>
+
+      <p>
+        Dalam pelaksanaan Tata Kelola Perusahaan yang baik, Unit Audit Internal
+        mempunyai peran penting dalam melakukan penilaian terhadap kecukupan
+        pengendalian internal, kepatuhan terhadap peraturan, dengan demikian
+        pengendalian internal menjadi bagian yang terintegrasi dalam sistem dan
+        prosedur pada setiap kegiatan di unit kerja sehingga setiap penyimpangan
+        dapat diketahui secara dini sehingga dapat dilakukan langkah perbaikan
+        oleh unit kerja yang bersangkutan. Unit Audit Internal senantiasa
+        melakukan pengawasan internal dengan melakukan pendekatan sistematis
+        agar penerapan prinsip prinsip Tata Kelola Perusahaan yang baik dapat
+        berjalan sesuai secara baik dan benar.
+      </p>
+    </>
+  );
+};
+
+const tatakelolaitem = [
+  { title: "Komite Audit", content: <Komiteaudit /> },
+  { title: "Komite Nominasi dan Remunerasi", content: <Komitenominasi /> },
+  { title: "Seketaris Perusahaan", content: <Sekretaris /> },
+  { title: "Unit Audit Internal", content: <Unitauditinternal /> },
+];
+
+export default tatakelolaitem;
